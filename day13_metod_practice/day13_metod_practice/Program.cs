bd7c5c54fa2c9f52e50d34ab1e246dd1b482da7f
elmar <elmar@elmar-Air.bbrouter>
2024-01-09 12:28:01 +0400

    yerine yetirildi

---
+++ day13_metod_practice/day13_metod_practice/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace day13_metod_practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] arr = new int[] { 3, 6, 7 };
+        int[] arr1 = new int[5];
+        Array.Copy(arr, 0, arr1, 2, 2);
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            Console.WriteLine(arr1[i]);
+
+        }
+
+        arr.CopyTo(arr1, 2);
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            Console.WriteLine(arr1[i]);
+
+        }
+
+        int[] array = new int[] { 56, 34, 8, 1, 67 };
+        Array.Sort(array);
+        Array.Reverse(array);
+        Array.Resize(ref array, 2);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+
+        }
+
+        int[] nums = new int[] { 2, 3, 4, 5, 6, 7, 5, 8 };
+
+        Console.WriteLine(Array.IndexOf(nums, 12));
+        Console.WriteLine(Array.IndexOf(nums, 3));
+        Console.WriteLine(Array.LastIndexOf(nums, 5));
+
+
+
+        string temp = " salam neterisiz ? PB302 ";
+
+        Console.WriteLine(temp.LastIndexOf('a'));
+        Console.WriteLine(temp.IndexOf('a'));
+        Console.WriteLine(temp.ToLower());
+        Console.WriteLine(temp.ToUpper());
+        Console.WriteLine(temp.Replace(' ','q'));
+
+        Console.WriteLine(temp.Contains("salam"));
+        Console.WriteLine(temp.Contains(" sa"));
+        Console.WriteLine(temp.EndsWith(' '));
+        Console.WriteLine(temp.EndsWith('1'));
+        Console.WriteLine(temp.StartsWith(' '));
+        Console.WriteLine(temp.StartsWith('s'));
+        Console.WriteLine(temp.Trim());
+
+
+
+        var newtemp = temp.Substring(0);
+        newtemp = temp.Substring(2, 6);
+        newtemp = temp.Substring(temp.IndexOf('s'), 5);
+        newtemp = temp.Substring(temp.IndexOf('r'));
+
+        for (int i = 0; i < newtemp.Length; i++)
+        {
+            Console.Write(newtemp[i]);
+
+        }
+
+        var spl = temp.Split(' ');
+        for (int i = 0; i < spl.Length; i++)
+        {
+            Console.WriteLine(spl[i]);
+
+        }
+        var jo = String.Join('0', spl);
+        Console.WriteLine(jo);
+
+
+
+        char let = 'A';
+
+        Console.WriteLine(char.ToUpper(let));
+        Console.WriteLine(char.ToLower(let));
+        Console.WriteLine(char.IsDigit(let));
+        Console.WriteLine(char.IsUpper(let));
+        Console.WriteLine(char.IsLetter(let));
+        Console.WriteLine(char.IsLetterOrDigit(let));
+        Console.ReadLine();
+    }
+}
+
---
+++ day13_tasks/day13_tasks/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace day13_tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Verilmiş email dəyərinin domain hissəni qaytaran metod
+        //  (elchin@code.edu.az email - i daxil edilsə code.edu.az hiss'sini qaytarmalıdır)
+        string str = "elchin@code.edu.az";
+        Console.WriteLine(domain(str));
+
+        // Verilmiş yazının yalnız hərflərdən ibarət olub olmadığını tapan metod
+
+        string text = "salam  menim 18 yasim var.";
+        Console.WriteLine(check_letter(text));
+
+        //Verilmiş yazının ilk hərfini böyük qalanlarını kiçik edib qaytaran metod
+        // (Misaçün "saLAm nEceSen" yazısı daxil edilsə metoddan "Salam necesen" return olmalıdır)
+
+        string text1 = "salaM MeNiM AdIm ELmARDIR";
+        Console.WriteLine(change_first_bigger(text1));
+        //Verilmiş fullnamelər siyahısından name-lər siyahısı düzəldən metod
+        // (ad soyadlar siyahısındaki adları kəsib yeni bir arraye doldurub qaytarmalıdır)
+        string[] fullName = new string[] { "elmar qarayev", "cavid quliyev", "ilham ibrahimov", "kenan tagiyev" };
+
+        string[] temp = names(fullName);
+        for (int i = 0; i < temp.Length; i++)
+        {
+            Console.WriteLine(temp[i]);
+        }
+        //Doğum tarixinizi daxil etdiyinizdə sizə yaşınızı qaytaran metod
+        //void funksiya ile
+        Birthday();
+        // return type string  olan bir funksiya ile
+        Console.WriteLine(Birthdayt("03 15  2005"));
+
+
+        Console.ReadLine();
+    }
+
+    static string domain(string str)
+    {
+        return str.Substring(str.IndexOf('@')+1);
+
+    }
+    static bool check_letter(string str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsDigit(str[i])) return false;
+        }
+        return true;
+    }
+    static string change_first_bigger(string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+        {
+            return str;
+        }
+       
+        StringBuilder result = new StringBuilder(str);
+        result[0] = char.ToUpper(result[0]);
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (char.IsUpper(str[i]))
+            {
+                result[i] = char.ToLower(str[i]);
+            }
+        }
+        return result.ToString();
+    }
+    static string[] names(string[] str)
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            str[i].Trim();
+            str[i] = str[i].Substring(0, str[i].IndexOf(' '));
+        }
+        return str;
+
+    }
+    static void Birthday()
+    {
+        string birth;
+        DateTime date;
+        do
+        {
+            Console.WriteLine("dogum tarixinizi qeyd edin:");
+            birth = Console.ReadLine();
+
+        } while (!DateTime.TryParse(birth, out date));
+
+        var diff =DateTime.Now.Year-date.Year;
+        if (diff < 0)
+        {
+            Console.WriteLine("siz hele dogulmayibsiniz");
+        }
+        else
+        {
+            Console.WriteLine( $"sizin {diff} yasiniz var");
+        }   
+    }
+
+    static string Birthdayt(string str)
+    {
+        
+        DateTime date;
+        if(DateTime.TryParse(str,out date))
+        {
+            int diff = DateTime.Now.Year - date.Year;
+            if (diff < 0)
+            {
+                return "siz hele dogulmayibsiniz!";
+            }
+            else
+                return $"sizin {diff.ToString()} yasiniz var";
+        }
+        else
+        {
+            return "yanlisidir zehmet olmasa duzgun daxil edin!";
+        }
+
+    }
+
+}
+
